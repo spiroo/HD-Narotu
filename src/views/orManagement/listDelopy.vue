5ed72a191317c3d149d825c6c76a28b585b2f240
Garyguosha <1687603275@qq.com>
2019-04-30 17:27:58 +0800

    submit orManagement file listDelopy

--- src/views/orManagement/listDelopy.vue
+++ src/views/orManagement/listDelopy.vue
@@ -1,7 +1,19 @@
  <template>
   <div class="listConfig">
     <div v-for="item in list" v-bind:key="item.delopy">
-      <div class="pank"><span>{{item.delopy}}</span><input class="add" type="button" name="add" value="添加"></div>
+      <div class="clow">
+        <span class="pank">
+          {{item.delopy}}
+          <input class="add" type="button" name="add" value="添加">
+        </span>
+      </div>
+      <div class="content">
+        <span  v-for="itemChild in item.delopyList" :key="itemChild.index">
+          <span class="delopydata">{{itemChild.message}}</span>
+        </span>
+         <span><el-input class="sizeWidth" v-model="input" placeholder="请输入添加内容"></el-input></span>
+      </div>
+     
     </div>
   </div>
 </template>
@@ -12,27 +24,66 @@
 export default {
   data() {
     return {
-      index:0,
+      index: 0,
+      input:"",
       list: [
-        { delopy: "职级", delopyList: ["正部级", "副部级"] },
-        { delopy: "党内职务", delopyList: ["党小组", "支部书记"] },
-        { delopy: "离退休时单位名称", delopyList: ["火箭员"] },
-        { delopy: "职务", delopyList: ["处长"] }
+        {
+          delopy: "职级",
+          delopyList: [
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+            { message: "正部级" },
+            { message: "副部级" },
+            { message: "正部级" },
+          ]
+        },
+        {
+          delopy: "党内职务",
+          delopyList: [{ message: "党小组" }, { message: "支部书记" }]
+        },
+        {
+          delopy: "离退休时单位名称",
+          delopyList: [{ message: "火箭员" }]
+        },
+        {
+          delopy: "职务",
+          delopyList: [{ message: "处长" }]
+        }
       ]
     };
   },
   // mounted() {
   //   this.getListDelopy();
   // },
-  methods:{
-  //   getListDelopy() {
-  //     const params = {
-
-  //     };
-  //     listDelopy(params).then(response => {
-  //       console.log("ll"+response)
-  //     });
-  //   }
+  methods: {
+    //   getListDelopy() {
+    //     const params = {
+    //     };
+    //     listDelopy(params).then(response => {
+    //       console.log("ll"+response)
+    //     });
+    //   }
   }
 };
 </script>
@@ -41,22 +92,31 @@ export default {
   position: absolute;
   padding-top: 30px;
   padding-left: 20px;
- 
+}
+.clow {
+  background-color: rgb(233, 231, 231);
+  width: 1450px;
+  line-height: 60px;
+  text-align: left;
 }
 .pank {
-   background-color:darkgrey; 
-   width: 800px;
-}
-.pank span{
-  /* float: left; */
-  margin-left:0px;
-
 }
 .add {
-  /* float: left; */
-  color: red;
-  font-size: 12px;
+  background-color: rgb(233, 231, 231);
   margin-left: 10px;
-  padding-bottom: 0px;
+  color: royalblue;
+}
+.content {
+  text-align: left;
+   width: 1445px;
+  border: 2px rgb(151, 146, 146) solid;
+}
+.delopydata {
+  margin-left: 30px;
+  line-height: 50px;
+}
+.sizeWidth {
+  width: "20px";
+  height: 50px;
 }
 </style>
